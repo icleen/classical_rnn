265faa2c0fffda19624e4fbe75a81bcaca4ba671
Brian Rasmussen <brian.rasmussen55@gmail.com>
2018-04-04 21:09:27 -0600

    added midi

---
+++ midiToJson/index.js
@@ -0,0 +1,17 @@
+const MidiParse = require("midiconvert");
+const fs = require('fs');
+var dir = "./B2";
+var dirJson = dir + "JSON"
+if (!fs.existsSync(dirJson)){
+    fs.mkdirSync(dirJson);
+}
+// const fs = require('fs');
+const MidiConvert = require('midiconvert');
+
+fs.readdirSync(`${dir}`).forEach(file => {
+    let midiBlob = fs.readFileSync(`${dir}/${file}`, "binary");
+    let json = MidiConvert.parse(midiBlob);
+    fs.writeFileSync(`${dirJson}/${file}.json`, JSON.stringify(json, null, 4));
+});
+
+
